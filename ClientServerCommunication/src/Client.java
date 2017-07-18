4d16df8648bd80e32f801706c635dda816564fba
Dan <curlyocto@gmail.com>
2017-07-18 08:03:49 -0500

    ClientServer Skeleton

---
+++ ClientServerCommunication/src/Client.java
@@ -0,0 +1,15 @@
+/**
+ * Created by dan on 7/17/17.
+ * This is the client side of the communication
+ */
+
+import java.net.Socket;
+public class Client {
+    public static void main(String[] args) throws Exception{
+
+
+
+    }
+
+
+}
---
+++ ClientServerCommunication/src/Server.java
@@ -0,0 +1,6 @@
+/**
+ * Created by dan on 7/17/17.
+ * This is the server side, listens on port 8990
+ */
+public class Server {
+}
