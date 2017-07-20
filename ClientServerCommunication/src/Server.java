14a8b1db2e133b778bad3daaffcbcf9c5fa1ff60
Dan <curlyocto@gmail.com>
2017-07-20 18:27:11 -0500

    Added code to server side

--- ClientServerCommunication/src/Server.java
+++ ClientServerCommunication/src/Server.java
@@ -1,6 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by dan on 7/17/17.
  * This is the server side, listens on port 8990
  */
 public class Server {
+
+
+    public static void main(String[] args) throws IOException {
+       // int portNumber = Integer.parseInt(args[0]);
+
+        try {
+
+            {
+                ServerSocket serverSocket = new ServerSocket(8900);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            }
+
+        }catch(IOException e){
+            System.out.println(e);
+        }
+
+
+    }
 }
