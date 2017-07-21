daaa614440753e3b9afd3dff2727d86f2313b8ba
UnrealSpeedie <speedieftw@gmail.com>
2017-07-21 17:33:11 -0500

    Added data variables and functions
    Added send/receive functions (un-implemented), outbound and inbound data
    variables, and client state (blocking/non-blocking) variable

--- ClientServerCommunication/src/Client.py
+++ ClientServerCommunication/src/Client.py
@@ -3,58 +3,79 @@ import socket
 class Client():
 
     #Server data
-    server_ip = ""
-    server_port = 0
+    _server_ip = ""
+    _server_port = 0
 
     #Client's socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    _client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     #Client's State Data
-    is_connected = False
+    _is_connected = False
+    _is_blocking = False
 
+    #Client's Data Stacks
+    _to_send = []
+    _to_receive = []
+    
     #Client's constructor
     def __init__(self, ip, port):
-        self.server_ip = ip
-        self.server_port = port
+        self._server_ip = ip
+        self._server_port = port
 
     #A simple wrapper function that connects to a sever
     def start_connect(self):
         try:
             print("Connecting...")
-            self.client_socket.connect((self.server_ip, self.server_port))
+            self._client_socket.connect((self.server_ip, self.server_port))
         except:
             print("Could not connect.")
         else:
-            self.is_connected = True
+            self._is_connected = True
             print("Connected")
-            
+
+    #A simple wrapper function to put outbound data on the send stack
+    def send(self, data):
+        #TODO: Implement
+        pass
+
+    #A simple wrapper function to put inbound data on the receive stack
+    def receive(self):
+        #TODO: Implement
+        pass
+    
     #A simple wrapper function that gracefully closes the connection
     def close_connection(self):
-        self.client_socket.close()
-        self.is_connected = False
+        self._client_socket.close()
+        self._is_connected = False
         
     #Getters
     def get_server_ip(self):
-        return self.server_ip
+        return self._server_ip
 
     def get_server_port(self):
-        return self.server_port
+        return self._server_port
 
     def get_connection_status(self):
-        return self.is_connected
+        return self._is_connected
+
+    def get_blocking_state(self):
+        return self._is_blocking
 
     #Setters
     def set_server_ip(self, ip):
-        self.server_ip = ip
+        self._server_ip = ip
 
     def set_server_port(self, port):
-        self.server_port = port
+        self._server_port = port
 
     def set_connection_status(self, status):
-        self.is_connected
+        self._is_connected
+
+    def set_blocking_state(self, blocking):
+        self._is_blocking = blocking
     
 if __name__ == '__main__':
-    client = Client("127.0.0.1", 8990)
+    client = Client("100.112.57.238", 8990)
     client.start_connect()
     client.close_connection()
 
