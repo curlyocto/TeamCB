b0a9e13da26dff9ef876ebc0d82584efdb4e5af3
UnrealSpeedie <speedieftw@gmail.com>
2017-07-22 23:18:58 -0500

    Updated names, added functionality/Implementation
    Updated variable names to better reflect how they will be used when
    functions are implemented.
    
    Added functions and implementations to use the newly added data stack
    variables.
    
    Added more comments and demonstration code.

--- ClientServerCommunication/src/Client.py
+++ ClientServerCommunication/src/Client.py
@@ -14,19 +14,20 @@ class Client():
     _is_blocking = False
 
     #Client's Data Stacks
-    _to_send = []
-    _to_receive = []
+    _send_stack = []
+    _receive_stack = ["Hello World!"]
     
     #Client's constructor
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, state = False):
         self._server_ip = ip
         self._server_port = port
+        self._is_blocking = state
 
     #A simple wrapper function that connects to a sever
-    def start_connect(self):
+    def start_connection(self):
         try:
             print("Connecting...")
-            self._client_socket.connect((self.server_ip, self.server_port))
+            self._client_socket.connect((self._server_ip, self._server_port))
         except:
             print("Could not connect.")
         else:
@@ -34,14 +35,19 @@ class Client():
             print("Connected")
 
     #A simple wrapper function to put outbound data on the send stack
+    #Data is appended(pushed) on to the rear of the stack
     def send(self, data):
-        #TODO: Implement
-        pass
+        self._send_stack.append(data)
 
     #A simple wrapper function to put inbound data on the receive stack
+    #Data is read from index 0 and then popped off the front of the stack
     def receive(self):
-        #TODO: Implement
-        pass
+        try:
+            data = self._receive_stack[0]
+            self._receive_stack.pop(0)
+            return data
+        except:
+            print("No data on receive stack.")
     
     #A simple wrapper function that gracefully closes the connection
     def close_connection(self):
@@ -72,15 +78,34 @@ class Client():
         self._is_connected
 
     def set_blocking_state(self, blocking):
-        self._is_blocking = blocking
+        if blocking:
+            self._is_blocking = True
+            self._client_socket.setblocking(True)
+        else:
+            self._is_blocking = False
+            
     
 if __name__ == '__main__':
-    client = Client("100.112.57.238", 8990)
-    client.start_connect()
+    #Creation of the client object
+    client = Client("127.0.0.1", 8990, False)
+
+    #Starts the socket connection
+    client.start_connection()
+
+    '''
+    #An example of popping receive data from the receive stack
+    print(client._receive_stack)
+    print(client.receive())
+    print(client._receive_stack)
+    '''
+
+    '''
+    #An example of pushing send data to the send stack
+    print(client._send_stack)
+    client.send("Hello World!")
+    print(client._send_stack)
+    ''' 
+
+    #Used to gracefully close the connction
     client.close_connection()
-
-    while(client.get_connection_status()):
-        print("Connected")
-
-
         
